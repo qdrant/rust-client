fc84a28758d86a235e403c1ee69abb752997096b
timvisee <tim@visee.me>
2023-03-06 17:10:25 +0100

    Extract README example to example binary

---
+++ examples/search.rs
@@ -0,0 +1,91 @@
+use anyhow::Result;
+use qdrant_client::prelude::*;
+use qdrant_client::qdrant::vectors_config::Config;
+use qdrant_client::qdrant::{CreateCollection, SearchPoints, VectorParams, VectorsConfig};
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Example of top level client
+    // You may also use tonic-generated client from `src/qdrant.rs`
+    let config = QdrantClientConfig::from_url("http://localhost:6334");
+    let client = QdrantClient::new(Some(config)).await?;
+
+    let collections_list = client.list_collections().await?;
+    dbg!(collections_list);
+    // collections_list = ListCollectionsResponse {
+    //     collections: [
+    //         CollectionDescription {
+    //             name: "test",
+    //         },
+    //     ],
+    //     time: 1.78e-6,
+    // }
+
+    let collection_name = "test";
+    client.delete_collection(collection_name).await?;
+
+    client
+        .create_collection(&CreateCollection {
+            collection_name: collection_name.into(),
+            vectors_config: Some(VectorsConfig {
+                config: Some(Config::Params(VectorParams {
+                    size: 10,
+                    distance: Distance::Cosine.into(),
+                })),
+            }),
+            ..Default::default()
+        })
+        .await?;
+
+    let collection_info = client.collection_info(collection_name).await?;
+    dbg!(collection_info);
+
+    let payload: Payload = vec![("foo", "Bar".into()), ("bar", 12.into())]
+        .into_iter()
+        .collect::<HashMap<_, Value>>()
+        .into();
+
+    let points = vec![PointStruct::new(0, vec![12.; 10], payload)];
+    client
+        .upsert_points_blocking(collection_name, points, None)
+        .await?;
+
+    let search_result = client
+        .search_points(&SearchPoints {
+            collection_name: collection_name.into(),
+            vector: vec![11.; 10],
+            filter: None,
+            limit: 10,
+            with_vectors: None,
+            with_payload: None,
+            params: None,
+            score_threshold: None,
+            offset: None,
+            ..Default::default()
+        })
+        .await?;
+    dbg!(search_result);
+    // search_result = SearchResponse {
+    //     result: [
+    //         ScoredPoint {
+    //             id: Some(
+    //                 PointId {
+    //                     point_id_options: Some(
+    //                         Num(
+    //                             0,
+    //                         ),
+    //                     ),
+    //                 },
+    //             ),
+    //             payload: {},
+    //             score: 1.0000001,
+    //             version: 0,
+    //             vectors: None,
+    //         },
+    //     ],
+    //     time: 5.312e-5,
+    // }
+
+    Ok(())
+}
