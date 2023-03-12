ca3e688a923cda276a4bd7a19159753971f819eb
Max Wertheim <minecart321@gmail.com>
2023-03-12 18:08:53 -0400

    Removed autogenerated files. Added conditional build for proto structs for serde feature.

--- build.rs
+++ build.rs
@@ -1,14 +1,20 @@
 fn main() -> std::io::Result<()> {
     if std::env::var("DOCS_RS").is_err() {
-        return tonic_build::configure()
-            .compile_well_known_types(true)
-            .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
-            .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
-            .out_dir("src/") // saves generated structures at this location
-            .compile(
-                &["proto/qdrant.proto"], // proto entry point
-                &["proto"],              // specify the root location to search proto dependencies
-            );
+        #[allow(unused_mut)]
+        let mut tonic_build_conf = tonic_build::configure().out_dir("src/"); // saves generated structures at this location
+
+        #[cfg(feature = "serde")]
+        {
+            tonic_build_conf = tonic_build_conf
+                .compile_well_known_types(true)
+                .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
+                .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp");
+        }
+
+        return tonic_build_conf.compile(
+            &["proto/qdrant.proto"], // proto entry point
+            &["proto"],              // specify the root location to search proto dependencies
+        );
     }
     Ok(())
 }
