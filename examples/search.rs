b07444a3a70915654e983b43309b0a6f8f8f11e4
Andre Bogus <bogusandre@gmail.com>
2023-06-09 18:52:04 +0200

    More docs'n'helpers

--- examples/search.rs
+++ examples/search.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
 use qdrant_client::prelude::*;
 use qdrant_client::qdrant::vectors_config::Config;
-use qdrant_client::qdrant::{CreateCollection, SearchPoints, VectorParams, VectorsConfig};
+use qdrant_client::qdrant::{
+    Condition, CreateCollection, Filter, SearchPoints, VectorParams, VectorsConfig,
+};
 use serde_json::json;
 
 #[tokio::main]
@@ -62,7 +64,7 @@ async fn main() -> Result<()> {
         .search_points(&SearchPoints {
             collection_name: collection_name.into(),
             vector: vec![11.; 10],
-            filter: None,
+            filter: Some(Filter::all([Condition::matches("bar", 12)])),
             limit: 10,
             with_vectors: None,
             with_payload: None,
