17f8ff5206b1c3243f3eeb35409f338d08f937f3
jojii <jojii@gmx.net>
2024-04-26 10:28:49 +0200

    add some From<T> impls for gprc types

--- src/grpc_ext.rs
+++ src/grpc_ext.rs
@@ -1,13 +1,20 @@
 use crate::client::Payload;
+use crate::qdrant::payload_index_params::IndexParams;
 use crate::qdrant::point_id::PointIdOptions;
 use crate::qdrant::points_selector::PointsSelectorOneOf;
 use crate::qdrant::value::Kind;
 use crate::qdrant::vectors::VectorsOptions;
 use crate::qdrant::with_payload_selector::SelectorOptions;
 use crate::qdrant::{
-    shard_key, with_vectors_selector, ListValue, NamedVectors, PayloadIncludeSelector, PointId,
-    PointStruct, PointsIdsList, PointsSelector, ShardKey, ShardKeySelector, SparseIndices, Struct,
-    Value, Vector, Vectors, VectorsSelector, WithPayloadSelector, WithVectorsSelector,
+    alias_operations, quantization_config, quantization_config_diff, shard_key, value,
+    vectors_config, vectors_config_diff, with_vectors_selector, AliasOperations,
+    BinaryQuantization, CreateAlias, DeleteAlias, Disabled, IntegerIndexParams, ListValue,
+    NamedVectors, PayloadIncludeSelector, PayloadIndexParams, PointId, PointStruct, PointsIdsList,
+    PointsSelector, ProductQuantization, QuantizationConfigDiff, RenameAlias, ScalarQuantization,
+    ShardKey, ShardKeySelector, SparseIndexConfig, SparseIndices, SparseVectorConfig,
+    SparseVectorParams, Struct, TextIndexParams, Value, Vector, VectorParams, VectorParamsDiff,
+    VectorParamsDiffMap, VectorParamsMap, Vectors, VectorsConfig, VectorsConfigDiff,
+    VectorsSelector, WithPayloadSelector, WithVectorsSelector,
 };
 use std::collections::HashMap;
 
@@ -298,5 +305,177 @@ impl PointStruct {
             payload: payload.into(),
             vectors: Some(vectors.into()),
         }
+    }
+}
+
+impl From<HashMap<String, VectorParams>> for VectorParamsMap {
+    fn from(value: HashMap<String, VectorParams>) -> Self {
+        VectorParamsMap { map: value }
+    }
+}
+
+impl From<HashMap<String, VectorParamsDiff>> for VectorParamsDiffMap {
+    fn from(value: HashMap<String, VectorParamsDiff>) -> Self {
+        VectorParamsDiffMap { map: value }
+    }
+}
+
+impl From<vectors_config::Config> for VectorsConfig {
+    fn from(value: vectors_config::Config) -> Self {
+        VectorsConfig {
+            config: Some(value),
+        }
+    }
+}
+
+impl From<VectorParams> for vectors_config::Config {
+    fn from(value: VectorParams) -> Self {
+        Self::Params(value)
+    }
+}
+
+impl From<VectorParamsMap> for vectors_config::Config {
+    fn from(value: VectorParamsMap) -> Self {
+        Self::ParamsMap(value)
+    }
+}
+
+impl From<vectors_config_diff::Config> for VectorsConfigDiff {
+    fn from(value: vectors_config_diff::Config) -> Self {
+        Self {
+            config: Some(value),
+        }
+    }
+}
+
+impl From<VectorParamsDiff> for vectors_config_diff::Config {
+    fn from(value: VectorParamsDiff) -> Self {
+        Self::Params(value)
+    }
+}
+
+impl From<VectorParamsDiffMap> for vectors_config_diff::Config {
+    fn from(value: VectorParamsDiffMap) -> Self {
+        Self::ParamsMap(value)
+    }
+}
+
+impl From<SparseIndexConfig> for SparseVectorParams {
+    fn from(value: SparseIndexConfig) -> Self {
+        Self { index: Some(value) }
+    }
+}
+
+impl From<HashMap<String, SparseVectorParams>> for SparseVectorConfig {
+    fn from(value: HashMap<String, SparseVectorParams>) -> Self {
+        Self { map: value }
+    }
+}
+
+impl From<ScalarQuantization> for quantization_config::Quantization {
+    fn from(value: ScalarQuantization) -> Self {
+        Self::Scalar(value)
+    }
+}
+
+impl From<ProductQuantization> for quantization_config::Quantization {
+    fn from(value: ProductQuantization) -> Self {
+        Self::Product(value)
+    }
+}
+
+impl From<BinaryQuantization> for quantization_config::Quantization {
+    fn from(value: BinaryQuantization) -> Self {
+        Self::Binary(value)
+    }
+}
+
+impl From<quantization_config_diff::Quantization> for QuantizationConfigDiff {
+    fn from(value: quantization_config_diff::Quantization) -> Self {
+        Self {
+            quantization: Some(value),
+        }
+    }
+}
+
+impl From<ScalarQuantization> for quantization_config_diff::Quantization {
+    fn from(value: ScalarQuantization) -> Self {
+        Self::Scalar(value)
+    }
+}
+
+impl From<ProductQuantization> for quantization_config_diff::Quantization {
+    fn from(value: ProductQuantization) -> Self {
+        Self::Product(value)
+    }
+}
+
+impl From<Disabled> for quantization_config_diff::Quantization {
+    fn from(value: Disabled) -> Self {
+        Self::Disabled(value)
+    }
+}
+
+impl From<BinaryQuantization> for quantization_config_diff::Quantization {
+    fn from(value: BinaryQuantization) -> Self {
+        Self::Binary(value)
+    }
+}
+
+impl From<IndexParams> for PayloadIndexParams {
+    fn from(value: IndexParams) -> Self {
+        Self {
+            index_params: Some(value),
+        }
+    }
+}
+
+impl From<TextIndexParams> for IndexParams {
+    fn from(value: TextIndexParams) -> Self {
+        Self::TextIndexParams(value)
+    }
+}
+
+impl From<IntegerIndexParams> for IndexParams {
+    fn from(value: IntegerIndexParams) -> Self {
+        Self::IntegerIndexParams(value)
+    }
+}
+
+impl From<alias_operations::Action> for AliasOperations {
+    fn from(value: alias_operations::Action) -> Self {
+        AliasOperations {
+            action: Some(value),
+        }
+    }
+}
+
+impl From<CreateAlias> for alias_operations::Action {
+    fn from(value: CreateAlias) -> Self {
+        Self::CreateAlias(value)
+    }
+}
+
+impl From<RenameAlias> for alias_operations::Action {
+    fn from(value: RenameAlias) -> Self {
+        Self::RenameAlias(value)
+    }
+}
+
+impl From<DeleteAlias> for alias_operations::Action {
+    fn from(value: DeleteAlias) -> Self {
+        Self::DeleteAlias(value)
+    }
+}
+
+impl From<HashMap<String, Value>> for Struct {
+    fn from(value: HashMap<String, Value>) -> Self {
+        Self { fields: value }
+    }
+}
+
+impl From<Kind> for Value {
+    fn from(value: Kind) -> Self {
+        Self { kind: Some(value) }
     }
 }
