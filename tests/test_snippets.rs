9b384cf9b6095a6c098f2865f60e12037fe10884
generall <andrey@vasnetsov.com>
2024-06-19 18:59:41 +0200

    add disclaimer

--- tests/test_snippets.rs
+++ tests/test_snippets.rs
@@ -1,3 +1,7 @@
 // Skip formatting for this file
 #[rustfmt::skip]
 mod snippet_tests;
+
+
+// WARNING: Snippet tests are not checking if the request is executing correctly.
+// The only thing it is checking is if the code compiles and runs without panicking.
