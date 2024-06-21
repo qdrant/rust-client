d19ce47ba40580dcc22f9ab00998989df617ec4b
Tim Visée <tim@visee.me>
2024-06-21 09:33:29 +0200

    Expose Payload at root, hide obsolete modules, vendor some types along in qdrant module (#139)
    * Expose Payload object at root of crate
    
    * Expose custom ReocmmendExample builder in qdrant types module
    
    * Vendor timestamp through qdrant types module, hide auth module
    
    * Improve documentation

--- src/qdrant.rs
+++ src/qdrant.rs
@@ -8705,3 +8705,5 @@ builder_type_conversions!(RenameAlias, RenameAliasBuilder, true);
 builder_type_conversions!(DeletePoints, DeletePointsBuilder, true);
 
 pub use crate::manual_builder::*;
+pub use crate::builder_types::*;
+pub use prost_types::Timestamp;
