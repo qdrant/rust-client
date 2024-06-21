7b0d2b38995e13243f46758b17adc4bb1536b3a8
Jojii <15957865+JojiiOfficial@users.noreply.github.com>
2024-06-21 11:50:40 +0200

    Add missing functionality and builder (#140)
    * add missing functionallity and builder
    
    * rename functions; add uuid conversion

--- src/builder_types.rs
+++ src/builder_types.rs
@@ -1,6 +1,6 @@
+use crate::qdrant::point_id::PointIdOptions;
 use crate::qdrant::{PointId, Vector};
 
-/// A recommendation example, being a [`PointId`] or a [`Vector`]
 pub enum RecommendExample {
     PointId(PointId),
     Vector(Vector),
@@ -15,5 +15,35 @@ impl From<Vector> for RecommendExample {
 impl From<PointId> for RecommendExample {
     fn from(value: PointId) -> Self {
         Self::PointId(value)
+    }
+}
+
+impl From<u64> for RecommendExample {
+    fn from(value: u64) -> Self {
+        Self::PointId(PointId {
+            point_id_options: Some(PointIdOptions::Num(value)),
+        })
+    }
+}
+
+impl From<&str> for RecommendExample {
+    fn from(value: &str) -> Self {
+        Self::PointId(PointId {
+            point_id_options: Some(PointIdOptions::Uuid(value.to_string())),
+        })
+    }
+}
+
+impl From<String> for RecommendExample {
+    fn from(value: String) -> Self {
+        Self::PointId(PointId {
+            point_id_options: Some(PointIdOptions::Uuid(value)),
+        })
+    }
+}
+
+impl From<Vec<f32>> for RecommendExample {
+    fn from(value: Vec<f32>) -> Self {
+        Self::Vector(value.into())
     }
 }
